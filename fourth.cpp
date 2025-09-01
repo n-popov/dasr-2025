9ef32c7efdc041d72c1bdabb3e491003c7b9c787
n-popov <68807935+n-popov@users.noreply.github.com>
2025-09-02 00:43:47 +0300

    Add files via upload

---
+++ fourth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n, m;
+
+    std::cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            std::cout << '+';
+        }
+        std::cout << std::endl;
+    }
+}
